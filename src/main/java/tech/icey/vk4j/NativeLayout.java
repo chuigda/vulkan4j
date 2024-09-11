aa7fa47ffed1f0afade03a46a2737066c9743bf5
icey <icey@icey.tech>
2024-09-12 02:49:13 +0800

    腾云驾雾 追风逐电 一个跟头 十万八千

--- src/main/java/tech/icey/vk4j/NativeLayout.java
+++ src/main/java/tech/icey/vk4j/NativeLayout.java
@@ -3,11 +3,34 @@ package tech.icey.vk4j;
 import java.lang.foreign.ValueLayout;
 
 public final class NativeLayout {
-    // TODO: detect pointer size at runtime to support both 32bit and 64bit, Windows and Linux
-    public static final int POINTER_SIZE = 8;
-    public static final int CLONG_SIZE = 8;
-    public static final ValueLayout C_LONG = ValueLayout.JAVA_LONG;
-
-    // TODO: I don't know if this is correct
     public static final ValueLayout C_SIZE_T = ValueLayout.ADDRESS;
+    public static final int POINTER_SIZE = (int) C_SIZE_T.byteSize();
+
+    public static final ValueLayout C_LONG;
+    public static final int C_LONG_SIZE;
+
+    static {
+        // See:
+        //
+        // https://en.cppreference.com/w/cpp/language/types#Data_models
+        // https://github.com/LWJGL/lwjgl3/blob/813400f21ebfce5a9e1566cbf8ff96ca1d8f4921/modules/lwjgl/core/src/main/java/org/lwjgl/system/Pointer.java#L34
+        if (POINTER_SIZE == 4) {
+            // On typical 32bit platforms, long is 4 bytes
+            C_LONG = ValueLayout.JAVA_INT;
+        }
+        else if (POINTER_SIZE == 8) {
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                // On 64bit Windows, long is 4 bytes
+                C_LONG = ValueLayout.JAVA_INT;
+            } else {
+                // Otherwise, long is 8 bytes
+                C_LONG = ValueLayout.JAVA_LONG;
+            }
+        }
+        else {
+            throw new RuntimeException(String.format("unsupported pointer size: %d", POINTER_SIZE));
+        }
+
+        C_LONG_SIZE = (int) C_LONG.byteSize();
+    }
 }
