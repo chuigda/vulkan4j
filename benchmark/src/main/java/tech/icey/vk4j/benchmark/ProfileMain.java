577de8f358d00d44dbeb1e7e384a9884c39460fc
CousinZe <alex@ckx-ily.cn>
2024-09-25 12:59:08 +0800

    Add more profiling items

--- benchmark/src/main/java/tech/icey/vk4j/benchmark/ProfileMain.java
+++ benchmark/src/main/java/tech/icey/vk4j/benchmark/ProfileMain.java
@@ -2,10 +2,12 @@ package tech.icey.vk4j.benchmark;
 
 import org.lwjgl.system.MemoryStack;
 import tech.icey.vk4j.Create;
+import tech.icey.vk4j.datatype.VkExtensionProperties;
 import tech.icey.vk4j.handle.VkPhysicalDevice;
 import tech.icey.vk4j.ptr.IntPtr;
 
 import java.lang.foreign.Arena;
+import java.nio.ByteBuffer;
 
 public class ProfileMain {
     static {
@@ -28,7 +30,7 @@ public class ProfileMain {
     }
 
     private static void vk4jMain() {
-        for (int i = 0; i < 40960; i++) {
+        for (int i = 0; i < 4096; i++) {
             try (Arena arena = Arena.ofConfined()) {
                 var pPhysicalDeviceCount = IntPtr.allocate(arena);
                 VK4JStatic.instanceCommands.vkEnumeratePhysicalDevices(VK4JStatic.instance, pPhysicalDeviceCount, null);
@@ -37,15 +39,22 @@ public class ProfileMain {
                 var pPhysicalDevices = Create.createArray(VkPhysicalDevice.FACTORY, arena, physicalDeviceCount).first;
                 VK4JStatic.instanceCommands.vkEnumeratePhysicalDevices(VK4JStatic.instance, pPhysicalDeviceCount, pPhysicalDevices[0]);
 
-                for (var device : pPhysicalDevices) {
-                    device.handle();
+                var pExtensionCount = IntPtr.allocate(arena);
+                VK4JStatic.instanceCommands.vkEnumerateDeviceExtensionProperties(pPhysicalDevices[0], null, pExtensionCount, null);
+                var extensionCount = pExtensionCount.read();
+
+                var pExtensionProperties = Create.createArray(VkExtensionProperties.FACTORY, arena, extensionCount).first;
+                VK4JStatic.instanceCommands.vkEnumerateDeviceExtensionProperties(pPhysicalDevices[0], null, pExtensionCount, pExtensionProperties[0]);
+                for (var extension : pExtensionProperties) {
+                    extension.extensionName();
+                    extension.specVersion();
                 }
             }
         }
     }
 
     private static void lwjglMain() {
-        for (int i = 0; i < 40960; i++) {
+        for (int i = 0; i < 4096; i++) {
             try (MemoryStack memoryStack = MemoryStack.stackPush()) {
                 var pPhysicalDeviceCount = memoryStack.mallocInt(1);
                 org.lwjgl.vulkan.VK10.vkEnumeratePhysicalDevices(LWJGLStatic.instance, pPhysicalDeviceCount, null);
@@ -59,8 +68,16 @@ public class ProfileMain {
                     physicalDevice[j] = new org.lwjgl.vulkan.VkPhysicalDevice(pPhysicalDevices.get(j), LWJGLStatic.instance);
                 }
 
-                for (var device : physicalDevice) {
-                    device.address();
+                var pExtensionCount = memoryStack.mallocInt(1);
+                org.lwjgl.vulkan.VK10.vkEnumerateDeviceExtensionProperties(physicalDevice[0], (ByteBuffer) null, pExtensionCount, null);
+                var extensionCount = pExtensionCount.get(0);
+
+                var pExtensionProperties = org.lwjgl.vulkan.VkExtensionProperties.calloc(extensionCount);
+                org.lwjgl.vulkan.VK10.vkEnumerateDeviceExtensionProperties(physicalDevice[0], (ByteBuffer) null, pExtensionCount, pExtensionProperties);
+                for (int j = 0; j < extensionCount; j++) {
+                    var extensionProperties = pExtensionProperties.get(j);
+                    extensionProperties.extensionName();
+                    extensionProperties.specVersion();
                 }
             }
         }
