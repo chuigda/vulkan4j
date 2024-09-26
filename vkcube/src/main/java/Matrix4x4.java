4046f8d4851f19d9c6d887950936a40f55c798f2
CousinZe <alex@ckx-ily.cn>
2024-09-26 14:29:43 +0800

    等待着谁 能够将我的心房轻轻叩击

--- vkcube/src/main/java/Matrix4x4.java
+++ vkcube/src/main/java/Matrix4x4.java
@@ -3,10 +3,37 @@ import tech.icey.vk4j.buffer.FloatBuffer;
 import java.lang.foreign.Arena;
 
 public final class Matrix4x4 {
-    public float m00, m01, m02, m03;
-    public float m10, m11, m12, m13;
-    public float m20, m21, m22, m23;
-    public float m30, m31, m32, m33;
+    public final float m00, m01, m02, m03;
+    public final float m10, m11, m12, m13;
+    public final float m20, m21, m22, m23;
+    public final float m30, m31, m32, m33;
+
+    public Matrix4x4(
+            float m00, float m01, float m02, float m03,
+            float m10, float m11, float m12, float m13,
+            float m20, float m21, float m22, float m23,
+            float m30, float m31, float m32, float m33
+    ) {
+        this.m00 = m00;
+        this.m01 = m01;
+        this.m02 = m02;
+        this.m03 = m03;
+
+        this.m10 = m10;
+        this.m11 = m11;
+        this.m12 = m12;
+        this.m13 = m13;
+
+        this.m20 = m20;
+        this.m21 = m21;
+        this.m22 = m22;
+        this.m23 = m23;
+
+        this.m30 = m30;
+        this.m31 = m31;
+        this.m32 = m32;
+        this.m33 = m33;
+    }
 
     public FloatBuffer toBuffer(Arena arena) {
         var ret = FloatBuffer.allocate(arena, 16);
@@ -31,5 +58,81 @@ public final class Matrix4x4 {
         ret.write(15, m33);
 
         return ret;
+    }
+
+    public Matrix4x4 dup() {
+        return new Matrix4x4(
+            m00, m01, m02, m03,
+            m10, m11, m12, m13,
+            m20, m21, m22, m23,
+            m30, m31, m32, m33
+        );
+    }
+
+    public Matrix4x4 rotateY(float angle) {
+        float cos = (float) Math.cos(angle);
+        float sin = (float) Math.sin(angle);
+        return new Matrix4x4(
+            m00 * cos + m02 * sin, m01, m00 * -sin + m02 * cos, m03,
+            m10 * cos + m12 * sin, m11, m10 * -sin + m12 * cos, m13,
+            m20 * cos + m22 * sin, m21, m20 * -sin + m22 * cos, m23,
+            m30 * cos + m32 * sin, m31, m30 * -sin + m32 * cos, m33
+        );
+    }
+
+    private Matrix4x4 orthoNormalize() {
+        Vector3 x = new Vector3(m00, m10, m20).normalize();
+        Vector3 y = new Vector3(m01, m11, m21).normalize();
+        Vector3 z = new Vector3(m02, m12, m22).normalize();
+
+        return new Matrix4x4(
+            x.x, y.x, z.x, m03,
+            x.y, y.y, z.y, m13,
+            x.z, y.z, z.z, m23,
+            m30, m31, m32, m33
+        );
+    }
+
+    public static Matrix4x4 IDENTITY = new Matrix4x4(
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    public static Matrix4x4 ZERO = new Matrix4x4(
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f
+    );
+
+    public static Matrix4x4 multiply(Matrix4x4 lhs, Matrix4x4 rhs) {
+        float m00 = lhs.m00 * rhs.m00 + lhs.m01 * rhs.m10 + lhs.m02 * rhs.m20 + lhs.m03 * rhs.m30;
+        float m01 = lhs.m00 * rhs.m01 + lhs.m01 * rhs.m11 + lhs.m02 * rhs.m21 + lhs.m03 * rhs.m31;
+        float m02 = lhs.m00 * rhs.m02 + lhs.m01 * rhs.m12 + lhs.m02 * rhs.m22 + lhs.m03 * rhs.m32;
+        float m03 = lhs.m00 * rhs.m03 + lhs.m01 * rhs.m13 + lhs.m02 * rhs.m23 + lhs.m03 * rhs.m33;
+
+        float m10 = lhs.m10 * rhs.m00 + lhs.m11 * rhs.m10 + lhs.m12 * rhs.m20 + lhs.m13 * rhs.m30;
+        float m11 = lhs.m10 * rhs.m01 + lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31;
+        float m12 = lhs.m10 * rhs.m02 + lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32;
+        float m13 = lhs.m10 * rhs.m03 + lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33;
+
+        float m20 = lhs.m20 * rhs.m00 + lhs.m21 * rhs.m10 + lhs.m22 * rhs.m20 + lhs.m23 * rhs.m30;
+        float m21 = lhs.m20 * rhs.m01 + lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31;
+        float m22 = lhs.m20 * rhs.m02 + lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32;
+        float m23 = lhs.m20 * rhs.m03 + lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33;
+
+        float m30 = lhs.m30 * rhs.m00 + lhs.m31 * rhs.m10 + lhs.m32 * rhs.m20 + lhs.m33 * rhs.m30;
+        float m31 = lhs.m30 * rhs.m01 + lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31;
+        float m32 = lhs.m30 * rhs.m02 + lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32;
+        float m33 = lhs.m30 * rhs.m03 + lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33;
+
+        return new Matrix4x4(
+            m00, m01, m02, m03,
+            m10, m11, m12, m13,
+            m20, m21, m22, m23,
+            m30, m31, m32, m33
+        );
     }
 }
