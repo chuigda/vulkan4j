67f0c4087915c37f88917b1f2bdd31c889cf8d4b
Chuigda <icey@icey.tech>
2024-10-03 23:35:24 +0800

    我从前相信 这世上有一个温暖的人

---
+++ glfw/src/main/java/tech/icey/glfw/GLFWLoader.java
@@ -0,0 +1,39 @@
+package tech.icey.glfw;
+
+import tech.icey.glfwmini.LibGLFW;
+import tech.icey.panama.FunctionLoader;
+import tech.icey.panama.Loader;
+
+public final class GLFWLoader {
+    /// Try loading the GLFW library.
+    ///
+    /// This function is implemented in terms of {@link System#loadLibrary}. On Windows it will try {@code "glfw3"}
+    /// ({@code glfw3.dll}), and on other platforms it will try {@code "glfw"} ({@code libglfw.so}).
+    ///
+    /// Instead of using this function, you may also implement your own GLFW library loading logic.
+    ///
+    /// @throws SecurityException see {@link System#loadLibrary}
+    /// @throws UnsatisfiedLinkError see {@link System#loadLibrary}
+    public static void loadGLFWLibrary() {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.loadLibrary("glfw3");
+        } else {
+            System.loadLibrary("glfw");
+        }
+    }
+
+    /// Load GLFW functions.
+    ///
+    /// This function is implemented in terms of {@link Loader#loadFunctionOrNull}. If any of the functions is not
+    /// found, that "slot" will be filled with {@code null}. Calling a function not loaded will result in a
+    /// {@link NullPointerException}, intuitively. For GLFW, in theory only platform-dependent functions (functions in
+    /// {@code glfw3native.h}) may not be found.
+    ///
+    /// Instead of using this function, you may also implement your own command loading logic with {@link GLFW#GLFW}
+    /// and your own {@link FunctionLoader}.
+    ///
+    /// @return loaded GLFW functions
+    public static LibGLFW loadGLFW() {
+        return new LibGLFW(Loader::loadFunctionOrNull);
+    }
+}
