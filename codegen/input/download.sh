4264772d6dd4fe856c8682fc91015c693c196cca
CousinZe <alex@ckx-ily.cn>
2024-10-20 17:01:36 +0800

    Heartaches, heartaches, my loving you meant only heartaches.

--- codegen/input/download.sh
+++ codegen/input/download.sh
@@ -8,6 +8,9 @@ wget https://raw.githubusercontent.com/glfw/glfw/refs/tags/3.4/include/GLFW/glfw
 echo Downloading Vulkan vk.xml
 wget https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/main/xml/vk.xml
 
+echo Downloading OpenGL gl.xml
+wget https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/main/xml/gl.xml
+
 # Note: we're restricting to VMA 3.1. Update this when VMA releases a new version.
 echo Downloading VMA header file
 wget https://raw.githubusercontent.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/refs/tags/v3.1.0/include/vk_mem_alloc.h
