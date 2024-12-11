65d5ddf29328907f2cd89d6d766b47043cd10da7
CousinZe <alex@ckx-ily.cn>
2024-12-11 21:53:00 +0800

    Correctly use module system

---
+++ gles2/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+@SuppressWarnings("JavaModuleNaming")
+module gles2 {
+    exports tech.icey.gles2;
+
+    requires tech.icey.panama;
+    requires org.jetbrains.annotations;
+}
--- vk4j/src/main/java/module-info.java
+++ vk4j/src/main/java/module-info.java
@@ -3,8 +3,8 @@ module tech.icey.vk4j {
     exports tech.icey.vk4j.bitmask;
     exports tech.icey.vk4j.command;
     exports tech.icey.vk4j.datatype;
-    exports tech.icey.vk4j.handle;
     exports tech.icey.vk4j.enumtype;
+    exports tech.icey.vk4j.handle;
 
     requires tech.icey.panama;
     requires org.jetbrains.annotations;
