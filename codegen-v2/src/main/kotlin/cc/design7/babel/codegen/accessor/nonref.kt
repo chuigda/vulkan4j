1b0efef15f6774d24487879516e088915dcb0a7e
HoshinoTented <hoshinotented@qq.com>
2025-05-14 19:38:35 +0800

    structure: blade works

--- codegen-v2/src/main/kotlin/cc/design7/babel/codegen/accessor/nonref.kt
+++ codegen-v2/src/main/kotlin/cc/design7/babel/codegen/accessor/nonref.kt
@@ -1,38 +1,47 @@
 package cc.design7.babel.codegen.accessor
 
+import cc.design7.babel.codegen.FIELD_segment
+import cc.design7.babel.codegen.LayoutField
+import cc.design7.babel.codegen.fn
 import cc.design7.babel.ctype.CPlatformDependentIntType
 import cc.design7.babel.ctype.CType
-import cc.design7.babel.registry.Member
+import cc.design7.babel.util.Doc
+import cc.design7.babel.util.buildDoc
 
-fun generateNonRefAccessor(type: CType, member: Member): String = if (type is CPlatformDependentIntType) {
+fun generateNonRefAccessor(type: CType, member: LayoutField.Typed): Doc = if (type is CPlatformDependentIntType) {
     generatePlatformDependentIntAccessor(type, member)
 } else {
     generateFixedTypeAccessor(type, member)
 }
 
-private fun generatePlatformDependentIntAccessor(type: CPlatformDependentIntType, member: Member): String {
+private fun generatePlatformDependentIntAccessor(type: CPlatformDependentIntType, member: LayoutField.Typed): Doc {
     return when (type.cType) {
         "long" -> {
-            """
-            public long ${member.name}() {
-                return NativeLayout.readCLong(segment, OFFSET$${member.name});
+            buildDoc {
+                fn("public", "long", member.name) {
+                    +"return NativeLayout.readCLong($FIELD_segment, ${member.offsetName});"
+                }
+
+                +""
+
+                fn("public", "void", member.name, "long value") {
+                    +"NativeLayout.writeCLong($FIELD_segment, ${member.offsetName}, value);"
+                }
             }
-            
-            public void ${member.name}(long value) {
-                NativeLayout.writeCLong(segment, OFFSET$${member.name}, value);
-            }
-            """.trimIndent()
         }
         "size_t" -> {
-            """
-            public @unsigned long ${member.name}() {
-                return NativeLayout.readCSizeT(segment, OFFSET$${member.name});
+            buildDoc {
+                fn("public", "@unsigned long", member.name) {
+                    +"return NativeLayout.readCSizeT($FIELD_segment, ${member.offsetName});"
+                }
+
+                +""
+
+                // FIXME: no need to be `@unsigned long value`?
+                fn("public", "void", member.name, "long value") {
+                    +"NativeLayout.writeCSizeT($FIELD_segment, ${member.offsetName}, value);"
+                }
             }
-            
-            public void ${member.name}(long value) {
-                NativeLayout.writeCSizeT(segment, OFFSET$${member.name}, value);
-            }
-            """.trimIndent()
         }
         else -> {
             throw Exception("unsupported platform dependent int type $type")
@@ -40,7 +49,7 @@ private fun generatePlatformDependentIntAccessor(type: CPlatformDependentIntType
     }
 }
 
-private fun generateFixedTypeAccessor(type: CType, member: Member): String =
+private fun generateFixedTypeAccessor(type: CType, member: LayoutField.Typed): Doc {
     """
         public ${type.jType} ${member.name}() {
             return segment.get(LAYOUT$${member.name}, OFFSET$${member.name});
@@ -50,3 +59,16 @@ private fun generateFixedTypeAccessor(type: CType, member: Member): String =
             segment.set(LAYOUT$${member.name}, OFFSET$${member.name}, value);
         }
     """.trimIndent()
+
+    return buildDoc {
+        fn("public", type.jType, member.name) {
+            +"return $FIELD_segment.get(${member.layoutName}, ${member.offsetName});"
+        }
+
+        +""
+
+        fn("public", "void", member.name, "${type.jType} value") {
+            +"segment.set(${member.layoutName}, ${member.offsetName}, value);"
+        }
+    }
+}
