9b219ccb71f9d9e9985e4eea27274d91486d4794
Chuigda Whitegive <icey@icey.tech>
2025-05-18 23:17:14 +0800

    Add `package-info.java`

---
+++ vulkan/src/main/java/club/doki7/vulkan/bitmask/package-info.java
@@ -0,0 +1,35 @@
+/// Bitmask (flags) types of Vulkan API
+///
+/// ## Quick start
+///
+/// In {@code vulkan4j} ecosystem, we don't use Java class types to represent bitmask types.
+/// We will be still using Java integers ({@code int} or {@code long}), also
+/// {@link club.doki7.ffm.ptr} types when representing pointer to or array of bitmasks.
+///
+/// In order to tell what bitmask type an integral value represents, we use the annotation
+/// {@link club.doki7.ffm.annotation.enumtype}. For example:
+///
+/// {@snippet :
+/// public record VkBufferCreateInfo(MemorySegment segment) {
+///     @enumtype(VkBufferUsageFlags.class) int usage() {
+///         // implementation omitted
+///     }
+///
+///     void usage(@enumtype(VkBufferUsageFlags.class) int value) {
+///         // implementation omitted
+///     }
+///
+///     // ...
+/// }
+/// }
+///
+/// The {@link club.doki7.ffm.annotation.enumtype} annotation provides useful in-IDE and JavaDoc
+/// links, so you can Ctrl-Click on the annotation argument to jump to the definition of the bitmask
+/// type.
+///
+/// Also there's a handy {@code explain} function to explain a bitmask integral value. For example:
+///
+/// {@snippet : VkBufferUsageFlags.explain(someFlags) }
+///
+/// Have fun.
+package club.doki7.vulkan.bitmask;
---
+++ vulkan/src/main/java/club/doki7/vulkan/package-info.java
@@ -0,0 +1,6 @@
+/// Vulkan Java bindings implemented with FFM APIs
+///
+/// Vulkan is a new generation graphics and compute API that provides high-efficiency,
+/// cross-platform access to modern GPUs used in a wide variety of devices from PCs and consoles to
+/// mobile phones and embedded platforms.
+package club.doki7.vulkan;
