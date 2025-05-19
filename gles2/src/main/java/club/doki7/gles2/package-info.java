7da46f8d931d91f96e224a5a27639ec4864479a9
Chuigda Whitegive <icey@icey.tech>
2025-05-19 10:56:07 +0800

    所赖君子见机，达人知命

--- gles2/src/main/java/club/doki7/gles2/package-info.java
+++ gles2/src/main/java/club/doki7/gles2/package-info.java
@@ -1,1 +1,2 @@
+/// OpenGL ES2 Java bindings implemented with Java 22 FFM (Project Panama) APIs.
 package club.doki7.gles2;
--- vulkan/src/main/java/club/doki7/vulkan/package-info.java
+++ vulkan/src/main/java/club/doki7/vulkan/package-info.java
@@ -1,6 +1,2 @@
 /// Vulkan Java bindings implemented with Java 22 FFM (Project Panama) APIs.
-///
-/// Vulkan is a new generation graphics and compute API that provides high-efficiency,
-/// cross-platform access to modern GPUs used in a wide variety of devices from PCs and consoles to
-/// mobile phones and embedded platforms.
 package club.doki7.vulkan;
