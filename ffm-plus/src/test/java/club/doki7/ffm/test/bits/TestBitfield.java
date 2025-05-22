6a91f992c3e3876ceb7e1a2834fc17c16fd0ae5d
Chuigda Whitegive <icey@icey.tech>
2025-05-22 17:35:54 +0800

    Add test for bitfields (with the help of native libraries). (#35)

--- ffm-plus/src/test/java/club/doki7/ffm/test/bits/TestBitfield.java
+++ ffm-plus/src/test/java/club/doki7/ffm/test/bits/TestBitfield.java
@@ -1,8 +1,80 @@
 package club.doki7.ffm.test.bits;
 
+import club.doki7.ffm.Loader;
+import club.doki7.ffm.bits.BitfieldUtil;
+import club.doki7.ffm.ptr.BytePtr;
+import club.doki7.ffm.ptr.IntPtr;
 import org.junit.jupiter.api.Test;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
+
 public class TestBitfield {
+    static {
+        String userDir = System.getProperty("user.dir");
+        String path = userDir;
+        if (userDir.contains("vulkan4j")) {
+            path = userDir.substring(0, userDir.indexOf("vulkan4j") + "vulkan4j".length());
+        }
+        path += "/misc/test_binary/libbitfield.so";
+        System.load(path);
+    }
+
+    private static final FunctionDescriptor fnType = FunctionDescriptor.ofVoid(ValueLayout.ADDRESS);
+
     @Test
-    void test() {}
+    void test1() throws Throwable {
+        MethodHandle check1 = Loader.loadFunction("checkBitfield1", fnType);
+        MethodHandle check2 = Loader.loadFunction("checkBitfield2", fnType);
+        MethodHandle check3 = Loader.loadFunction("checkBitfield3", fnType);
+        MethodHandle check4 = Loader.loadFunction("checkBitfield4", fnType);
+
+        try (Arena arena = Arena.ofConfined()) {
+            BytePtr ptr = BytePtr.allocate(arena);
+            MemorySegment s = ptr.segment();
+
+            for (int i = 0; i < 8; i++) {
+                BitfieldUtil.writeBit(s, i, i % 2 == 0);
+            }
+            check1.invokeExact(s);
+
+            for (int i = 0; i < 8; i++) {
+                BitfieldUtil.writeBit(s, i, i % 2 != 0);
+            }
+            check2.invokeExact(s);
+
+            for (int i = 0; i < 4; i++) {
+                BitfieldUtil.writeBit(s, i, true);
+            }
+            for (int i = 4; i < 8; i++) {
+                BitfieldUtil.writeBit(s, i, false);
+            }
+            check3.invokeExact(s);
+
+            for (int i = 0; i < 4; i++) {
+                BitfieldUtil.writeBit(s, i, false);
+            }
+            for (int i = 4; i < 8; i++) {
+                BitfieldUtil.writeBit(s, i, true);
+            }
+            check4.invokeExact(s);
+        }
+    }
+
+    @Test
+    void test2() throws Throwable {
+        MethodHandle check5 = Loader.loadFunction("checkBitfield5", fnType);
+
+        try (Arena arena = Arena.ofConfined()) {
+            IntPtr ptr = IntPtr.allocate(arena);
+            MemorySegment s = ptr.segment();
+
+            BitfieldUtil.writeBits(s, 0, 24, 0x0D0007);
+            BitfieldUtil.writeBits(s, 24, 32, 0x21);
+            check5.invokeExact(s);
+        }
+    }
 }
---
+++ misc/test_binary/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -r *.o
+rm -r *.so
+
+for file in $(ls *.c); do
+  cc -c -g "$file" -o "${file%.c}.o" -fPIC
+  cc "${file%.c}.o" -shared -o "lib${file%.c}.so"
+done
