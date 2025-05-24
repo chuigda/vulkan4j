d5214bccd59580de4757858bccbccc9646b9a2ef
Chuigda Whitegive <chuigda@doki7.club>
2025-05-25 04:06:37 +0800

    This does not work but Y?

--- misc/test_binary/build.sh
+++ misc/test_binary/build.sh
@@ -7,3 +7,10 @@ for file in $(ls *.c); do
   cc -c -g "$file" -o "${file%.c}.o" -fPIC
   cc "${file%.c}.o" -shared -o "lib${file%.c}.so"
 done
+
+echo "Current working directory: $PWD"
+ls -l
+
+for file in $(ls *.so); do
+  chmod +x "$file"
+done
