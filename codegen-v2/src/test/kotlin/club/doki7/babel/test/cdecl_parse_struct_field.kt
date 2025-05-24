6a8d7ab423f90637d5dc56230fd039ab461050be
Chuigda Whitegive <chuigda@doki7.club>
2025-05-24 13:27:33 +0800

    增补测试

--- codegen-v2/src/test/kotlin/club/doki7/babel/test/cdecl_parse_struct_field.kt
+++ codegen-v2/src/test/kotlin/club/doki7/babel/test/cdecl_parse_struct_field.kt
@@ -1,8 +1,12 @@
 package club.doki7.babel.test
 
+import club.doki7.babel.cdecl.RawArrayType
+import club.doki7.babel.cdecl.RawIdentifierType
 import club.doki7.babel.cdecl.Tokenizer
 import club.doki7.babel.cdecl.parseStructFieldDecl
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class TestParseStructField {
     @Test
@@ -10,7 +14,14 @@ class TestParseStructField {
         val tokenizer = Tokenizer(listOf("PFN_vmaCheckDefragmentationBreakFunction VMA_NULLABLE pfnBreakCallback;"), 0)
         val structFieldDecl = parseStructFieldDecl(tokenizer)
 
-        println(structFieldDecl)
+        assertEquals("pfnBreakCallback", structFieldDecl.name)
+        assertTrue(structFieldDecl.syntaxTrivia.isEmpty())
+
+        val type = structFieldDecl.type
+        assertTrue(type is RawIdentifierType)
+        assertEquals("PFN_vmaCheckDefragmentationBreakFunction", type.ident)
+        assertEquals(1, type.syntaxTrivia.size)
+        assertEquals("VMA_NULLABLE", type.syntaxTrivia[0])
     }
 
     @Test
@@ -18,6 +29,15 @@ class TestParseStructField {
         val tokenizer = Tokenizer(listOf("VmaDetailedStatistics memoryType[VK_MAX_MEMORY_TYPES];"), 0)
         val structFieldDecl = parseStructFieldDecl(tokenizer)
 
-        println(structFieldDecl)
+        assertEquals("memoryType", structFieldDecl.name)
+        assertTrue(structFieldDecl.syntaxTrivia.isEmpty())
+        val type = structFieldDecl.type
+        assertTrue(type is RawArrayType)
+        assertEquals("VK_MAX_MEMORY_TYPES", type.size)
+        assertTrue(type.syntaxTrivia.isEmpty())
+        val elementType = type.element
+        assertTrue(elementType is RawIdentifierType)
+        assertEquals("VmaDetailedStatistics", elementType.ident)
+        assertTrue(elementType.syntaxTrivia.isEmpty())
     }
 }
