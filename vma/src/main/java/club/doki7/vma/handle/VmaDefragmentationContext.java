0a0ddc52fce5dc667ba1c406c8e796a1a58cae62
Chuigda Whitegive <chuigda@doki7.club>
2025-05-27 11:38:15 +0800

    Add generated code

--- vma/src/main/java/club/doki7/vma/handle/VmaDefragmentationContext.java
+++ vma/src/main/java/club/doki7/vma/handle/VmaDefragmentationContext.java
@@ -16,7 +16,7 @@ import club.doki7.ffm.annotation.*;
 /// ## Contracts
 ///
 /// The property {@link #segment()} should always be not-null
-/// (({@code segment != NULL && !segment.equals(MemorySegment.NULL)}), and properly aligned to
+/// ({@code segment != NULL && !segment.equals(MemorySegment.NULL)}), and properly aligned to
 /// {@link AddressLayout#byteAlignment()} bytes. To represent null pointer, you may use a Java
 /// {@code null} instead. See the documentation of {@link IPointer#segment()} for more details.
 ///
