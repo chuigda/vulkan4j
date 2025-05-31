f9efcb4c1b6f8b24ea4afcc060cfa1ccdcb0d04d
Chuigda Whitegive <chuigda@doki7.club>
2025-05-31 22:42:04 +0800

    lately I've been, I've been losing sleep, dreaming about things that we could be

---
+++ codegen-v2/src/test/kotlin/club/doki7/babel/test/util_common_prefix_postfix.kt
@@ -0,0 +1,50 @@
+package club.doki7.babel.test
+
+import club.doki7.babel.util.commonPostfix
+import club.doki7.babel.util.commonPrefix
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestCommonPrefixPostfix {
+    @Test
+    fun test1() {
+        val strings = listOf(
+            "LTO_SYMBOL_ALIGNMENT_MASK",
+            "LTO_SYMBOL_PERMISSIONS_MASK",
+            "LTO_SYMBOL_PERMISSIONS_CODE",
+            "LTO_SYMBOL_DEFINITION_UNDEFINED",
+            "LTO_SYMBOL_COMDAT"
+        )
+
+        assertEquals("LTO_SYMBOL_", commonPrefix(strings))
+        assertEquals("", commonPostfix(strings))
+    }
+
+    @Test
+    fun test2() {
+        val strings = listOf(
+            "LLVMBinaryTypeArchive",
+            "LLVMBinaryTypeOffload",
+            "LLVMBinaryTypeELF32L",
+            "LLVMBinaryTypeELF64B",
+            "LLVMBinaryTypeIR"
+        )
+
+        assertEquals("LLVMBinaryType", commonPrefix(strings))
+        assertEquals("", commonPostfix(strings))
+    }
+
+    @Test
+    fun testPostfix1() {
+        val strings = listOf(
+            "LLVMMDStringMetadataKind",
+            "LLVMDIDerivedTypeMetadataKind",
+            "LLVMDITemplateValueParameterMetadataKind",
+            "LLVMDIArgListMetadataKind",
+            "LLVMDIAssignIDMetadataKind"
+        )
+
+        assertEquals("LLVM", commonPrefix(strings))
+        assertEquals("MetadataKind", commonPostfix(strings))
+    }
+}
