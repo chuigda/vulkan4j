541ce38e88ab52d1f0f5939c10d8212f15f1359f
Chuigda Whitegive <chuigda@doki7.club>
2025-06-01 09:44:12 +0800

    But baby I've been, I've been praying hard

--- codegen-v2/input/download_llvm.sh
+++ codegen-v2/input/download_llvm.sh
@@ -1,40 +1,4 @@
 #!/usr/bin/env bash
 
-prefix="https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-20.1.6/llvm/include/llvm-c/"
-files=(
-  "Transforms/PassBuilder.h"
-  "Analysis.h"
-  "BitReader.h"
-  "BitWriter.h"
-  "Core.h"
-  "DataTypes.h"
-  "DebugInfo.h"
-  "Disassembler.h"
-  "DisassemblerTypes.h"
-  "Error.h"
-  "ErrorHandling.h"
-  "ExecutionEngine.h"
-  "IRReader.h"
-  "Linker.h"
-  "LLJIT.h"
-  "LLJITUtils.h"
-  "lto.h"
-  "Object.h"
-  "Orc.h"
-  "OrcEE.h"
-  "Remarks.h"
-  "Support.h"
-  "Target.h"
-  "TargetMachine.h"
-  "Types.h"
-)
-
-mkdir llvm
-mkdir llvm/Transforms
-
-for file in "${files[@]}"; do
-  url="${prefix}${file}"
-  download_location="./llvm/${file}"
-  echo "Downloading ${url}..."
-  curl -o "${download_location}" "${url}"
-done
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.6/llvm-20.1.6.src.tar.xz
+tar -xf llvm-20.1.6.src.tar.xz
