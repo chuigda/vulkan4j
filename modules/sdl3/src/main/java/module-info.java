f9848e49f1fc7fdd21c130f9fe69164b7b5e08f9
Chuigda Whitegive <chuigda@doki7.club>
2025-06-08 20:29:34 +0800

    Finish all these `export`s

--- modules/sdl3/src/main/java/module-info.java
+++ modules/sdl3/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module club.doki7.sdl3 {
     exports club.doki7.sdl3;
+    exports club.doki7.sdl3.bitmask;
+    exports club.doki7.sdl3.datatype;
+    exports club.doki7.sdl3.enumtype;
+    exports club.doki7.sdl3.handle;
 
     requires club.doki7.ffm;
     requires java.base;
