badbb67e21a8e9ac355df035b1fb0f36b54e6e01
Lyra <1170158548@qq.com>
2025-06-13 16:58:33 +0800

    Thinking.....

--- modules/codegen-v2/src/main/kotlin/club/doki7/babel/extract/webgpu/constantTypeMappings.kt
+++ modules/codegen-v2/src/main/kotlin/club/doki7/babel/extract/webgpu/constantTypeMappings.kt
@@ -14,7 +14,7 @@ val constantTypeMappings = mapOf(
     "UINT8_MAX" to ConstantTypeMapping("uint8_t", "0xFF", IdentifierType("short"), "(short)0xFF"),
     "UINT16_MAX" to ConstantTypeMapping("uint16_t", "0xFFFF", IdentifierType("int"), "0xFFFF"),
     "UINT32_MAX" to ConstantTypeMapping("uint32_t", "0xFFFFFFFF", IdentifierType("long"), "0xFFFFFFFFL"),
-    "UINT64_MAX" to ConstantTypeMapping("uint64_t", "0xFFFFFFFFFFFFFFFF", IdentifierType("long long"), "new BigInteger(\"FFFFFFFFFFFFFFFF\", 16)"),
+    "UINT64_MAX" to ConstantTypeMapping("uint64_t", "0xFFFFFFFFFFFFFFFF", IdentifierType("long long"), "0xFFFFFFFF_FFFFFFFFL"),
 
     "INT8_MAX" to ConstantTypeMapping("int8_t", "0x7F", IdentifierType("byte"), "127"),
     "INT8_MIN" to ConstantTypeMapping("int8_t", "-0x80", IdentifierType("byte"), "-128"),
@@ -26,7 +26,7 @@ val constantTypeMappings = mapOf(
     "INT64_MIN" to ConstantTypeMapping("int64_t", "-0x8000000000000000", IdentifierType("long"), "Long.MIN_VALUE"),
 
     "SIZE_MAX" to ConstantTypeMapping("size_t", "0xFFFFFFFF", IdentifierType("long"), "0xFFFFFFFFL"),
-    "USIZE_MAX" to ConstantTypeMapping("usize_t", "0xFFFFFFFFFFFFFFFF", IdentifierType("long long"), "new BigInteger(\"FFFFFFFFFFFFFFFF\", 16)"),
+    "USIZE_MAX" to ConstantTypeMapping("usize_t", "0xFFFFFFFFFFFFFFFF", IdentifierType("long long"), "0xFFFFFFFF_FFFFFFFFL"),
 
     "NAN" to ConstantTypeMapping("float", "NaN", IdentifierType("float"), "Float.NaN")
 )
