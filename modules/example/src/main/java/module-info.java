6a074ba68d0a393e95614b717ca001435d9718d0
Chuigda Whitegive <chuigda@doki7.club>
2025-06-17 00:04:53 +0800

    Fix `SDL` and `example` versions

--- modules/example/src/main/java/module-info.java
+++ modules/example/src/main/java/module-info.java
@@ -2,11 +2,8 @@ module example {
     requires club.doki7.ffm;
     requires club.doki7.openal;
     requires club.doki7.sdl3;
-<<<<<<< shaderc
     requires club.doki7.shaderc;
-=======
     requires club.doki7.vulkan;
     requires club.doki7.vma;
     requires java.desktop;
->>>>>>> master
 }
