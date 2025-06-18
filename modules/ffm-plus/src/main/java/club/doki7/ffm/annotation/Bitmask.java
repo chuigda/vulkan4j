3aa0d3ae5a0911461593f556262ca46128fbdaf8
cousinze <alex@ckx-ily.cn>
2025-06-18 16:21:38 +0800

    Update JavaDoc.

--- modules/ffm-plus/src/main/java/club/doki7/ffm/annotation/Bitmask.java
+++ modules/ffm-plus/src/main/java/club/doki7/ffm/annotation/Bitmask.java
@@ -2,7 +2,7 @@ package club.doki7.ffm.annotation;
 
 import java.lang.annotation.Documented;
 
-/// Marker annotation, indicating that an integral value is a C enumeration type.
+/// Marker annotation, indicating that an integral value is a C bitmask type.
 @Documented
 public @interface Bitmask {
     Class<?> value() default void.class;
