6a9185055bbf80247182f712773602885633624d
Chuigda Whitegive <chuigda@doki7.club>
2025-06-20 10:41:27 +0800

    Update STB download script, use `clang-format` as post-processor

--- modules/codegen-v2/input/download_stb.sh
+++ modules/codegen-v2/input/download_stb.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-curl -o stb_image.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
-curl -o stb_truetype.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_truetype.h
+STB_URL_PREFIX="https://raw.githubusercontent.com/nothings/stb/f58f558c120e9b32c217290b80bad1a0729fbb2c/"
+
+COMPONENTS=("image" "truetype" "image_resize2")
+
+mkdir -p stb
+mkdir -p stb_formatted
+for component in "${COMPONENTS[@]}"; do
+  curl -o "stb/stb_${component}.h" "${STB_URL_PREFIX}stb_${component}.h";
+  cat "stb/stb_${component}.h" | clang-format --style=file:.clang-format > "stb_formatted/stb_${component}.h";
+done
