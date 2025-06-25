e9c8ce662bb58a1c7b237eebd0dc256f1cc2e374
Chuigda Whitegive <chuigda@doki7.club>
2025-06-25 23:34:44 +0800

    Update `module-info.java`

--- modules/openxr/src/main/java/module-info.java
+++ modules/openxr/src/main/java/module-info.java
@@ -1,3 +1,19 @@
+/// **(Experimental)** OpenXR Java bindings implemented with Java22 FFM (Project Panama) APIs.
+///
+/// **This module is still experimental. It may contain bugs or incomplete features.**
+///
+/// For OpenXR reference, see <a href="https://www.khronos.org/registry/OpenXR/specs/1.1/html/xrspec.html">OpenXR Specification</a>.
+///
+/// ## Use in Maven
+///
+/// ```
+/// <dependency>
+///     <groupId>club.doki7</groupId>
+///     <artifactId>openxr</artifactId>
+///     <version>0.4.1</version>
+///     <scope>compile</scope>
+/// </dependency>
+/// ```
 module club.doki7.openxr {
     exports club.doki7.openxr;
     exports club.doki7.openxr.bitmask;
