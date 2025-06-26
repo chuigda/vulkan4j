2501806df8356d6e990e76ed6ff780e859d66034
Chuigda Whitegive <chuigda@doki7.club>
2025-06-26 18:35:46 +0800

    Update Unix side code and CHANGELOG.md

--- modules/ffm-plus/src/test/java/club/doki7/ffm/test/library/TestLoadFailure.java
+++ modules/ffm-plus/src/test/java/club/doki7/ffm/test/library/TestLoadFailure.java
@@ -1,7 +1,6 @@
 package club.doki7.ffm.test.library;
 
-import club.doki7.ffm.library.ILibraryLoader;
-import club.doki7.ffm.library.ISharedLibrary;
+import club.doki7.ffm.library.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
@@ -11,10 +10,23 @@ public final class TestLoadFailure {
     @Test
     @EnabledOnOs(OS.WINDOWS)
     void testWindows() {
-        try (ISharedLibrary _ = ILibraryLoader.platformLoader().loadLibrary("notexist")) {
+        try (ISharedLibrary _ = WindowsLibraryLoader.INSTANCE.loadLibrary("notexist")) {
             throw new AssertionError("The library should not exist");
         } catch (UnsatisfiedLinkError e) {
             Assertions.assertTrue(e.getMessage().contains("LoadLibraryW error: 126"));
+        }
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testLinux() {
+        try (ISharedLibrary _ = UnixLibraryLoader.INSTANCE.loadLibrary("notexist")) {
+            throw new AssertionError("The library should not exist");
+        } catch (UnsatisfiedLinkError e) {
+            String message = e.getMessage();
+            System.err.println(message);
+            Assertions.assertTrue(message.contains("dlopen error:"));
+            Assertions.assertFalse(message.contains("unknown error"));
         }
     }
 }
