9ebd60df9ef7666f51ba4a2b6b24d8046436b116
Chuigda Whitegive <chuigda@doki7.club>
2025-06-26 13:55:40 +0800

    Update Test to use `@EnableOnOs`, also cover Linux platform.

--- modules/ffm-plus/src/test/java/club/doki7/ffm/test/library/TestLoadFailure.java
+++ modules/ffm-plus/src/test/java/club/doki7/ffm/test/library/TestLoadFailure.java
@@ -1,21 +1,34 @@
 package club.doki7.ffm.test.library;
 
 import club.doki7.ffm.library.ISharedLibrary;
+import club.doki7.ffm.library.UnixLibrary;
+import club.doki7.ffm.library.UnixLibraryLoader;
 import club.doki7.ffm.library.WindowsLibraryLoader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 public final class TestLoadFailure {
     @Test
+    @EnabledOnOs(OS.WINDOWS)
     void testWindows() {
-        if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
-            return;
-        }
-
         try (ISharedLibrary _ = WindowsLibraryLoader.INSTANCE.loadLibrary("notexist")) {
             throw new AssertionError("The library should not exist");
         } catch (UnsatisfiedLinkError e) {
             Assertions.assertTrue(e.getMessage().contains("LoadLibraryW error: 126"));
+        }
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testLinux() {
+        try (ISharedLibrary _ = UnixLibraryLoader.INSTANCE.loadLibrary("nonexist")) {
+            throw new AssertionError("The library should not exist");
+        } catch (UnsatisfiedLinkError e) {
+            String message = e.getMessage();
+            System.err.println(message);
+            Assertions.assertTrue(message.contains("dlopen error:"));
         }
     }
 }
